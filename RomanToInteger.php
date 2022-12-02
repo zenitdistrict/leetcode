e4d15c03595c398fba07bd69c197ed3333652e15
Mamai, Raman <rmamai@scnsoft.com>
2022-12-02 19:12:55 +0300

    roman to integer

---
+++ RomanToInteger.php
@@ -0,0 +1,45 @@
+<?php
+
+class RomanToInteger {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        $romans = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1,
+        );
+        $result = 0;
+
+        foreach ($romans as $key => $value) {
+            while(true) {
+                if (strpos($s, $key) === 0) {
+                    $result += $value;
+                    $s = substr($s, strlen($key));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
+
+//Example:
+$converter = new RomanToInteger();
+echo $converter->romanToInt('MCMXCIV');
+//Output: 1994
